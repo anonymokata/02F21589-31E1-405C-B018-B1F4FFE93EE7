da9edfebe3879d5e05ca139e4e75294ae1297fff
anonymous <anonymous>
2017-01-15 16:55:58 -0600

    Main Updated until addition or subtraction operation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 	//Declar the required variables
 	int IpNoStatus = 0;
 	char IpRomanNo1[50], IpRomanNo2[50];
-	int RomanToDecimalNo1=0,RomanToDecimalNo2=0 ;
+	int RomanToDecimalNo1=0,RomanToDecimalNo2=0,Operation=0,Result=0;
 
 	//Scan the First Roman Number
 	printf("Enter First Roman No:");
@@ -34,14 +34,30 @@ int main()
 		IpNoStatus = IpCheck(IpRomanNo2);
 	}
 
+	//Select Operation 1= Addition and 2=Subtraction
+	printf("Enter 1 for Addition(+) OR Enter 2 for Subtraction(-):");
+	scanf("%d", &Operation);
+	
 	//Display Scaned Data
-	printf("First Number: %s\tSecond Number: %s\n",IpRomanNo1,IpRomanNo2);
+	if(Operation == 1)
+		printf("First Number: %s\tSecond Number: %s\tOperation selected:Addition\n",IpRomanNo1,IpRomanNo2);
+	else
+		printf("First Number: %s\tSecond Number: %s\tOperation selected:Subtraction\n",IpRomanNo1,IpRomanNo2);
 
 	//Convert Roman Number to Decimal
 	RomanToDecimalNo1 = RomanToDecimal(IpRomanNo1);
 	RomanToDecimalNo2 = RomanToDecimal(IpRomanNo2);
 	
 	//Display The converted Numbers
-	printf("First Number: %d\t Second Number: %d",RomanToDecimalNo1,RomanToDecimalNo2);
+	printf("First Number: %d\t Second Number: %d\n",RomanToDecimalNo1,RomanToDecimalNo2);
+
+	//Perform Add or Subtraction as per selection
+	Result = AddOrSub(RomanToDecimalNo1,RomanToDecimalNo2,Operation);
+
+	//Display The Final Result
+	if(Operation == 1)
+		printf("\nThe Addition is:%d\nThank You For Using My Roman Calc\n",Result);
+	else
+		printf("\nThe Subtraction is:%d\nThank You For Using My Roman Calc\n",Result);
 	return 0;
 }
